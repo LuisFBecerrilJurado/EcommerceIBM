980c6855d59b40683e443d794c558f8d240c5129
Fran <l.francisco.bj@gmail.com>
2024-01-03 15:01:37 -0600

    [NEW] creando repositorio de Proyecto Final

---
+++ BackEnd/CarShopping/src/main/java/com/example/CarShopping/CarShoppingEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarShopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShoppingEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShoppingEcommerceApplication.class, args);
+	}
+
+}
---
+++ BackEnd/Users/src/test/java/com/example/Users/UsersEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
